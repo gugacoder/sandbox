2275c4ab54ac954d510fc83133a38e0737681880
Guga Coder <guga.coder@gmail.com>
2020-11-20 19:05:51 -0300

    Correcao de erro na procedure de monitoramento de venda

--- .rascunho/_tmp_monitorar_integracao_de_venda.sql
+++ .rascunho/_tmp_monitorar_integracao_de_venda.sql
@@ -1,7 +1,3 @@
---
--- PROCEDURE dbo.monitorar_integracao
---
-
 use DBmercadologic;
 
 /*
@@ -19,7 +15,7 @@ drop procedure if exists replica._tmp_monitorar_integracao_de_venda_por_empresa
 go
 create procedure replica._tmp_monitorar_integracao_de_venda_por_empresa (
     @cod_empresa int
-  , @data_inicial date = null
+  , @data_inicial date
   -- Parâmetros opcionais de conectividade.
   -- Se omitidos os parâmetros são lidos da view replica.vw_empresa.
   , @provider nvarchar(50) = null
@@ -53,21 +49,23 @@ begin
 
   declare @sql nvarchar(max)
 
-  set @data_inicial = cast(coalesce(@data_inicial, current_timestamp) as date)
-
   set @sql = '
     select '+convert(varchar(100),@cod_empresa)+' as cod_empresa
+         , dia
          , id_cupom
          , id_item_cupom
       from openrowset(
            '''+@provider+'''
          , ''Driver='+@driver+';Server='+@servidor+';Port=5432;Database='+@database+';Uid='+@usuario+';Pwd='+@senha+';''
-         , ''select public.cupomfiscal.id as id_cupom
-                  , public.itemcupomfiscal.id as id_item_cupom
-               from public.cupomfiscal
-               left join public.itemcupomfiscal
-                      on itemcupomfiscal.idcupomfiscal = public.cupomfiscal.id
-              where public.cupomfiscal.dataabertura >= '''''+convert(varchar(100),@data_inicial,120)+''''';''
+         , ''select * from (
+               select cast(public.cupomfiscal.dataabertura as date) as dia
+                    , public.cupomfiscal.id as id_cupom
+                    , public.itemcupomfiscal.id as id_item_cupom
+                 from public.cupomfiscal
+                 left join public.itemcupomfiscal
+                        on itemcupomfiscal.idcupomfiscal = public.cupomfiscal.id
+                where public.cupomfiscal.dataabertura >= '''''+convert(varchar(100),@data_inicial,120)+'''''
+             ) as t;''
          ) as t'
 
   exec sp_executesql @sql
@@ -95,9 +93,12 @@ begin
   declare @cod_empresa int
   declare @tb_remoto table (
     cod_empresa int,
+    dia date,
     id_cupom int,
     id_item_cupom int
   )
+
+  set @data_inicial = cast(coalesce(@data_inicial, current_timestamp) as date)
 
   if @cod_empresas is not null
     insert into @tb_empresa select valor from replica.SPLIT(@cod_empresas,',')
@@ -116,13 +117,15 @@ begin
   ; with
   tb_total_remoto as (
     select cod_empresa
+         , dia
          , count(distinct id_cupom) as cupom
          , count(distinct id_item_cupom) as item_cupom
       from @tb_remoto
-     group by cod_empresa
+     group by cod_empresa, dia
   ),
   tb_total_local as (
     select replica.cupomfiscal.cod_empresa
+         , cast(replica.cupomfiscal.dataabertura as date) as dia
          , count(distinct replica.cupomfiscal.id) as cupom
          , count(distinct replica.itemcupomfiscal.id) as item_cupom
       from replica.cupomfiscal
@@ -130,18 +133,32 @@ begin
              on itemcupomfiscal.idcupomfiscal = replica.cupomfiscal.id
      where replica.cupomfiscal.dataabertura >= @data_inicial
      group by replica.cupomfiscal.cod_empresa
-  )
-  select tb_empresa.cod_empresa
+            , cast(replica.cupomfiscal.dataabertura as date)
+  ),
+  tb_total as (
+  select tb_total_local.cod_empresa
+       , tb_total_local.dia
        , tb_total_local.cupom as cupom_director
        , tb_total_remoto.cupom as cupom_concentrador
        , tb_total_local.item_cupom as item_cupom_director
        , tb_total_remoto.item_cupom as item_cupom_concentrador
+    from tb_total_local
+    full join tb_total_remoto
+           on tb_total_remoto.cod_empresa = tb_total_local.cod_empresa
+          and tb_total_remoto.dia = tb_total_local.dia
+  )
+  select tb_empresa.cod_empresa
+       , tb_total.dia
+       , tb_total.cupom_director
+       , tb_total.cupom_concentrador
+       , tb_total.item_cupom_director
+       , tb_total.item_cupom_concentrador
+       , cast(case when tb_total.item_cupom_director = tb_total.item_cupom_concentrador
+           then 1 else 0
+         end as bit) as ok
     from @tb_empresa as tb_empresa
-    left join tb_total_local
-           on tb_total_local.cod_empresa = tb_empresa.cod_empresa
-    left join tb_total_remoto
-           on tb_total_remoto.cod_empresa = tb_empresa.cod_empresa
-
+    left join tb_total
+           on tb_total.cod_empresa = tb_empresa.cod_empresa
 /*
   select @cod_empresa as cod_empresa, 'director' as origem, * from tb_total_local
   union
@@ -150,23 +167,5 @@ begin
 end
 go
 
-/*
+exec replica._tmp_monitorar_integracao_de_venda --4, '2020-11-20'
 
-  ; with
-  tb_total_remoto as (
-    select count(distinct id_cupom) as cupom
-         , count(distinct id_item_cupom) as item_cupom
-      from @tb_remoto
-  ),
-  tb_total_local as (
-    select count(distinct replica.cupomfiscal.id) as cupom
-         , count(distinct replica.itemcupomfiscal.id) as item_cupom
-      from replica.cupomfiscal
-      left join replica.itemcupomfiscal
-             on itemcupomfiscal.idcupomfiscal = replica.cupomfiscal.id
-     where replica.cupomfiscal.dataabertura >= cast(current_timestamp as date)
-  )
-  select @cod_empresa as cod_empresa, 'director' as origem, * from tb_total_local
-  union
-  select @cod_empresa as cod_empresa, 'concentrador' as origem, * from tb_total_remoto
-  */
